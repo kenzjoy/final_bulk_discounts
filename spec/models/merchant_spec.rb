3c9d0d18f924d3d24e7fbf4c17a8aca9d08b4f1f
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-14 22:23:24 -0700

    merchant relationships

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,14 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  describe 'relationships' do
-    it { should have_many(:items)}
-  end
-
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_length_of(:name).is_at_most(50) }
   end
+
+  describe 'relationships' do
+    it { should have_many :items }
+    it { should have_many(:invoice_items).through(:items) }
+    it { should have_many(:invoices).through(:invoice_items)}
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+    it { should have_many :bulk_discounts }
+  end
+
 
   describe 'methods' do
     before :each do
