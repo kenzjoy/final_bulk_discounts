7e9e00d5d738200b2631ee24a114da7396bf88cb
Mackenzie Leng <kenz.joy@outlook.com>
2022-11-14 22:29:37 -0700

    item relationships

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,14 @@
 class Item < ApplicationRecord
-  has_many :invoice_items, dependent: :destroy
-  has_many :invoices, through: :invoice_items
-  belongs_to :merchant
-
-  enum status: [ :disabled, :enabled ]
-
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true
   validates :unit_price, presence: true, numericality: true
+
+  has_many :invoice_items, dependent: :destroy
+  has_many :invoices, through: :invoice_items
+  belongs_to :merchant
+  has_many :bulk_discounts, through: :merchant
+
+  enum status: [ :disabled, :enabled ]
 
   def top_selling_date
     invoices
